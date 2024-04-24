90a1bf06ba63b3ff628eb24c5ae627187c81721d
n-tolegenov <nurbol.tolegenovv@gmail.com>
2024-04-24 17:28:12 +0600

    Enum для типа и статуса аккаунта: - AccountStatus - AccountType

---
+++ src/main/java/kz/almaty/finance/corebankingservice/model/AccountStatus.java
@@ -0,0 +1,5 @@
+package kz.almaty.finance.corebankingservice.model;
+
+public enum AccountStatus {
+    PENDING,ACTIVE,DORMANT,BLOCKED
+}
---
+++ src/main/java/kz/almaty/finance/corebankingservice/model/AccountType.java
@@ -0,0 +1,5 @@
+package kz.almaty.finance.corebankingservice.model;
+
+public enum AccountType {
+    SAVINGS_ACCOUNT, FIXED_DEPOSIT, LOAN_ACCOUNT
+}
